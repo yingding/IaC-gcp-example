73dd5495c03888cbe156e661dd947e2fa54b0f40
yingding <yingdingwang@yahoo.de>
2024-04-07 14:49:41 +0200

    updated api service enable

--- main.tf
+++ main.tf
@@ -28,6 +28,21 @@ provider "google" {
   zone        =  var.gcp_zone
 }
 
+# Activate services in the gcp project
+
+# Enable API Service on GCP for using gemini api
+resource "google_project_service" "vertex_ai_api" {
+  project = var.gcp_project_id
+  service = "aiplatform.googleapis.com"
+
+  timeouts {
+    create = "40m"
+    update = "50m"
+  }
+
+  disable_dependent_services = true
+}
+
 
 
 
